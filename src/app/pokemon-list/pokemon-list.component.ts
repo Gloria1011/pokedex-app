11aab46fd44ca1c4a218bfc01a7f9bed13f2d1ed
Gloria <ramogloria20@gmail.com>
2022-11-02 19:30:49 +0100

    deleted commets

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -8,7 +8,7 @@ import { DataService } from '../service/data.service';
 })
 export class PokemonListComponent implements OnInit {
   pokemons: any[] = []
-  // We get the current page(pagiantion),which is valid.
+  // We get the current page of pagiantion,which is valid.
   page = 1
   totalPokemons: number | any;
 
@@ -18,25 +18,37 @@ export class PokemonListComponent implements OnInit {
 
   ngOnInit(): void {
     this.getPokemons();
-}
 
-// get Pokemons method
+  }
 
-getPokemons(){
-  this.dataService.getPokemons(10, this.page + 0)
-  .subscribe((response: any) => {
-    this.totalPokemons = response.count;
-    
-// we loop through our pokemons
-    response.results.forEach((result: { name: string; }) => {
-      this.dataService.getMoreData(result.name)
-        .subscribe((uniqResponse: any) => {
-          this.pokemons.push(uniqResponse);
-          console.log(this.pokemons)
-        })
-    });
-  })
-}
+  // get Pokemons method
+
+  getPokemons() {
+
+    // FIRST CALL
+    this.dataService.getPokemons(10, this.page + 0)
+      // here we get the list with minimal data per pokemnon name
+      .subscribe((response: any) => {
+        // WHAT API GIVES US,THE OBJECT
+        this.totalPokemons = response.count;
+        // we loop through our pokemons
+        response.results.forEach((result: { name: string; }) => {
+
+          // SECOND CALL FOR MORE INFO
+          this.dataService.getMoreDataPerPokemon(result.name)
+            // here is a second http call to our rest pokemon api
+            // so we get more info per pokemon by name
+            .subscribe((uniqResponse: any) => {
+              console.log(uniqResponse)
+              this.pokemons.push(uniqResponse);
+              console.log(this.pokemons)
+            })
+
+
+
+        });
+      })
+  }
 
 }
 
--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -7,18 +7,14 @@ import { HttpClient } from '@angular/common/http';
 export class DataService {
 
   constructor(private http: HttpClient) { }
-
-
   // get pokemons from API
-
   getPokemons(limit: number, offset: number) {
     return this.http.get(`https://pokeapi.co/api/v2/pokemon?limit = ${limit} & offset=${offset}`);
   }
 
-// offeset value ,hw many nr to skip, then put those values you'll need, equals to the page+0
-
   // get more pokemons data
-  getMoreData(name: string){
+  getMoreDataPerPokemon(name: string){
      return this.http.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
   }
+
 }
